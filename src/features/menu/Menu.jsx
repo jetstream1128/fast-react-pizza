ecb140a010df96d73d89fe75bde64d512c420daa
jetstream1128 <bogdanalexeev2007@live.com>
2024-05-08 19:11:33 -0600

    added search order

--- src/features/menu/Menu.jsx
+++ src/features/menu/Menu.jsx
@@ -1,10 +1,11 @@
 import { useLoaderData } from "react-router-dom";
-import { getMenu } from "../../services/apiRestaurant";
+
 import MenuItem from "./MenuItem";
+import { getMenu } from "../../services/apiRestaurant";
 
 function Menu() {
 	const menu = useLoaderData();
-	console.log(menu);
+
 	return (
 		<ul>
 			{menu.map((pizza) => (
