f74855b7f2165003a7268c52d317f6d103bc42d6
jetstream1128 <bogdanalexeev2007@live.com>
2024-05-12 16:27:56 -0600

    styled order form

--- src/features/cart/Cart.jsx
+++ src/features/cart/Cart.jsx
@@ -35,17 +35,18 @@ function Cart() {
 
       <h2 className="mt-7 text-xl font-semibold">Your cart, %NAME%</h2>
 
-      <ul>
+      <ul className="mt-3 divide-y divide-stone-200 border-b">
         {cart.map((item) => (
           <CartItem item={item} key={item.key} />
         ))}
       </ul>
 
-      <div>
+      <div className="mt-6 space-x-2">
         <Button type="primary" to="/order/new">
           Order pizzas
         </Button>
-        <button>Clear cart</button>
+
+        <Button type="secondary">Clear cart</Button>
       </div>
     </div>
   );
