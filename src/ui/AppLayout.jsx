8720c96d173ea3ff580a6440a0fe6ce245f6203d
jetstream1128 <bogdanalexeev2007@live.com>
2024-05-12 18:51:55 -0600

    Finished styling

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -10,7 +10,6 @@ function AppLayout() {
   return (
     <div className="grid h-screen grid-rows-[auto_1fr_auto]">
       {isLoading && <Loader />}
-
       <Header />
       <div className="overflow-auto">
         <main className="mx-auto max-w-3xl">
--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -4,8 +4,8 @@ import Username from "../features/user/Username";
 
 function Header() {
   return (
-    <header className="flex items-center justify-between border-b border-stone-200 bg-yellow-500 px-4 py-3 uppercase sm:px-6">
-      <Link to="/" className=" tracking-widest">
+    <header className="relative flex h-14 items-center justify-between border-b border-stone-200 bg-yellow-500 px-4 py-3 uppercase sm:px-6">
+      <Link to="/" className="tracking-widest">
         Fast React Pizza Co.
       </Link>
 
